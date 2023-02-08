8db09ae74a4b9475e1b7e527e9ec030fdf18654b
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2023-02-08 17:56:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// While Statments
+// let tamer = 0;
+
+// while (tamer < 5) {
+//  // tamer++;  tamer = tamer +1;
+//     console.log(++tamer); // 1, 2, 3, 4, 5
+
+// }
+
+// do .. while
+// let i = 5;
+// do {
+//     console.log("I am wasta " + i);
+//     i++;
+// } while (i < 10);
+
+const array = [1, 25, 41, 7, 96];
+console.table(array);
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
+//
